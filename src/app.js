66ddc5b3899a34884099a2855d4d21016aa7e345
rohit1101 <srohit1101@gmail.com>
2021-05-15 16:25:21 +0530

    exploring expressjs methods

--- src/app.js
+++ src/app.js
@@ -1,12 +1,23 @@
-import { express } from "express";
+import express from "express";
 
 const app = express();
+const hostname = "127.0.0.1";
 const port = 3000;
 
 app.get("/", (req, res) => {
+  console.log(
+    req.url,
+    req.headers,
+    req.route,
+    req.params,
+    req.path,
+    req.method
+  );
+  res.statusCode = 200;
+  res.statusMessage = "OK express";
   res.send("Hello from express");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://127.0.0.1:${port}`);
 });
