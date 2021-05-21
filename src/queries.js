ada3995aa7b080983333e9debcbde9bce45d82a2
rohit1101 <srohit1101@gmail.com>
2021-05-21 21:34:23 +0530

    ignore .env

--- src/queries.js
+++ src/queries.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
 const { Pool, Client } = require("pg");
 
 const newPool = new Pool({
-  user: "admin",
-  host: "localhost",
-  database: "sales_api",
-  password: "password",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 console.log(newPool);
