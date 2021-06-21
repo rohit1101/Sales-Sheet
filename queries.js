a89e928e50fdff78b6d959332df8d57dae04c49b
rohit1101 <srohit1101@gmail.com>
2021-06-21 16:13:32 +0530

    PUT request expense entry refactor

--- queries.js
+++ queries.js
@@ -192,29 +192,41 @@ exports.addExpenseEntry = (req, res) => {
 exports.updateExpenseEntry = async (req, res) => {
   const { id } = req.params;
   const { date, amount_paid, description } = req.body;
-  let dbArgs = [];
-  amount_paid && dbArgs.push(parseFloat(amount_paid));
-  date && dbArgs.push(date);
-  description && dbArgs.push(description);
+  let dbArgs = Object.keys(req.body);
+  console.log(dbArgs);
+  let dbVals = [];
+  let query = "";
 
-  let query = `update expenses set 
-  ${amount_paid ? " amount_paid = $1 " : ""}
-  ${date ? " date = $2 " : ""}
-  ${description ? " description = $3 " : ""}
-  where id=${id}`.trim();
+  amount_paid && dbVals.push(+amount_paid);
+  date && dbVals.push(date);
+  description && dbVals.push(description);
+
+  if (dbArgs.length === 1) {
+    query = `update expenses set ${dbArgs[0]}=$1  where id=${id};`;
+  } else {
+    const updateStr = [...dbArgs]
+      .map((item, index) => item + `=$${index + 1}`)
+      .join(",");
+    console.log(updateStr);
+    query = `update expenses set ${updateStr} where id=${id};`;
+  }
+
+  console.log(query, dbArgs, dbVals);
 
   if (isNaN(id)) {
     return res.status(400).send("Invalid ID");
   }
 
   const doesIdExists = await pool
-    .query("SELECT EXISTS(SELECT 1 FROM sales WHERE id = $1)", [parseInt(id)])
+    .query("SELECT EXISTS(SELECT 1 FROM expenses WHERE id = $1)", [
+      parseInt(id),
+    ])
     .then((result) => result.rows[0].exists);
 
   if (doesIdExists) {
     return pool
-      .query(query, dbArgs)
-      .then(() => res.status(200).send(`Sales Entry modified with id:${id}`))
+      .query(query, dbVals)
+      .then(() => res.status(200).send(`Expenses Entry modified with id:${id}`))
       .catch((e) => console.log("Error PUT request =>", e));
   } else {
     return res.status(400).send("ID does not exist");
@@ -244,14 +256,3 @@ exports.deleteExpenseEntry = async (req, res) => {
     return res.status(400).send("ID does not exist");
   }
 };
-// ${
-//   amount_paid && date && card_id
-//     ? "amount_paid = $1,date = $2,card_id = $3"
-//     : ""
-// }
-// ${amount_paid && date ? "amount_paid = $1,date = $2" : ""}
-// ${date && card_id ? "date=$1,card_id=$2" : ""}
-// ${card_id && amount_paid ? "card_id=$1,amount_paid=$2" : ""}
-// ${amount_paid ? "amount_paid = $1" : ""}
-// ${date ? "date = $1" : ""}
-// ${card_id ? "card_id = $1" : ""}
