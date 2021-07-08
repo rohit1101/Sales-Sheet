7bf64615cc42a7028994a408f57e49eaaa774267
rohit1101 <srohit1101@gmail.com>
2021-07-08 15:56:03 +0530

    Update cors settings

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    // origin: "http://localhost:3000",
-    origin: "*",
+    origin: "http://localhost:3000",
     // credentials: true,
     // methods: ["GET", "POST"],
   })
