c71c31580a748ac97568bb732bbadf23da6f0c67
rohit1101 <srohit1101@gmail.com>
2021-07-17 13:29:30 +0530

    remove passport config

--- index.js
+++ index.js
@@ -18,26 +18,6 @@ const cors = require("cors");
 const { verifyToken } = require("./utils/tokens");
 const app = express();
 
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-
-passport.use(
-  new LocalStrategy(function (username, password, done) {
-    User.findOne({ username: username }, function (err, user) {
-      if (err) {
-        return done(err);
-      }
-      if (!user) {
-        return done(null, false, { message: "Incorrect username." });
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, { message: "Incorrect password." });
-      }
-      return done(null, user);
-    });
-  })
-);
-
 const requestTime = (req, res, next) => {
   let start = new Date().getTime();
   next();
